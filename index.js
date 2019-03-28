c98d7861b01044252de00f7f7613e6242158ee16
Nicole Diez <nicolediez@Nicoles-MacBook-Air.local>
2019-03-28 13:38:54 -0500

    Propinas

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const chalk = require('chalk');
-
-const nombre = 'Manning';
-console.log(chalk.green(nombre));
-
-const total = 15.99;
-console.log(chalk.green(total * 3));
+const impuesto = 0.12;
+let Nicole = 20;
+let Luis = 30;
+let Alessandra = 35;
+let itbms1 = Nicole *impuesto;
+let itbms2 = Luis *impuesto;
+let itbms3 = Alessandra *impuesto;
+let total = Nicole + itbms1 + Luis + itbms2 + Alessandra + itbms3
+console.log(total);
+let propina1 = 0;
+let propina2 = (Luis + itbms2) * 0.15;
+let propina3 = Alessandra *0.10;
+total = total + propina1 +propina2 +propina3;
+console.log(total);

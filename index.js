2ae95a11febe974b064c50453775e2283c115c1b
Nicole Diez <nicolediez@Nicoles-MacBook-Air.local>
2019-04-04 14:06:49 -0500

    Dinner with friends

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+function calculatetax(total,tax){
+    return total * tax;
+}
+
+function calculatetotal(consume,itbms){
+    return consume + itbms;
+}
+
+function calculatetip(consume,overtax,tip){
+    if (overtax === true){
+        const itbms = calculatetax(consume,0.07);
+        const total = calculatetotal(consume,itbms);
+        const totaltip = total * tip;
+        return total + totaltip; 
+    } else if(overtax === false){
+        const itbms = calculatetax(consume,0.07);
+        const total = calculatetotal(consume,itbms);
+        const totaltip = consume * tip;
+        return total + totaltip;
+    }
+
+}
 const impuesto = 0.12;
-let Nicole = 20;
-let Luis = 30;
-let Alessandra = 35;
-let itbms1 = Nicole *impuesto;
-let itbms2 = Luis *impuesto;
-let itbms3 = Alessandra *impuesto;
-let total = Nicole + itbms1 + Luis + itbms2 + Alessandra + itbms3
-console.log(total);
-let propina1 = 0;
-let propina2 = (Luis + itbms2) * 0.15;
-let propina3 = Alessandra *0.10;
-total = total + propina1 +propina2 +propina3;
-console.log(total);
+let Nicole = calculatetip(20, true, 0.10);
+let Luis = calculatetip(30, true, 0.15)
+let Alessandra = calculatetip(35, false, 0.10);
+
+console.log(Nicole);
+console.log(Luis);
+console.log(Alessandra);
